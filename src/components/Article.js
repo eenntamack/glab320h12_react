8ffcf383db2255484b48a456f7a89ba1692f1d4f
entamack1511 <entamack1511@gmail.com>
2025-04-29 23:39:51 -0400

    handling href to enable netlfy deployment

--- src/components/Article.js
+++ src/components/Article.js
@@ -10,7 +10,7 @@ export default function Article(){
                 Laboriosam, ea suscipit. Odio autem quisquam ipsa impedit similique saepe repellendus ullam maiores. Adipisci quibusdam, ullam, voluptatibus perspiciatis facilis libero nulla fuga architecto beatae explicabo nesciunt corrupti voluptas officiis blanditiis!
                 Corporis atque incidunt quia, repellendus tenetur suscipit accusamus et non. Ab nostrum voluptatum expedita ad deserunt neque voluptates dolorem modi delectus repudiandae ipsum suscipit, laborum voluptate fugit quae adipisci dignissimos.
                 Laboriosam dicta excepturi voluptatibus quis explicabo ad repellat ipsam perspiciatis aspernatur similique iure deleniti doloribus error neque recusandae eligendi est mollitia placeat fuga deserunt, eveniet sit delectus aut. Suscipit, temporibus!</p>
-                <a href="#">continues...</a>
+                <a href="/continue">continues...</a>
                 <hr/>   
             </article>
             
@@ -21,7 +21,7 @@ export default function Article(){
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam earum nobis eveniet perspiciatis. Itaque architecto sint, nobis animi quam mollitia magnam totam numquam quibusdam enim ad inventore dolores in alias.
                 Expedita deleniti repudiandae voluptatem dolore harum ea cumque ex, nisi distinctio rem assumenda corporis officia blanditiis vero unde praesentium consequuntur aliquam delectus earum quia illo odit consequatur facilis? Cum, ex?
                 Illum possimus tenetur est, nemo reiciendis neque accusantium excepturi iste. Eveniet itaque non magni eaque excepturi quos, blanditiis repellendus illum, dolore placeat unde officia vitae quasi impedit, id nulla ipsum.</p>
-                <a href="#">continues...</a>
+                <a href="/continue">continues...</a>
                 <hr/>   
             </article>
             
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,12 @@ export  function  HeaderNav(){
     return(
         <nav aria-label="Main Navigation" role="navigation" > 
                 <ul>
-                    <li><a href="#">Women's</a></li>
-                    <li><a href="#">Men's</a></li>
-                    <li><a href="#">On the Street</a></li>
-                    <li><a href="#">The Catwalk</a></li>
-                    <li><a href="#">AdWatch</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="/Women">Women's</a></li>
+                    <li><a href="/Men">Men's</a></li>
+                    <li><a href="/OnTheStreet">On the Street</a></li>
+                    <li><a href="/TheCatWalk">The Catwalk</a></li>
+                    <li><a href="/AdWatch">AdWatch</a></li>
+                    <li><a href="/About">About</a></li>
                 </ul>
         </nav>
     )
@@ -18,14 +18,14 @@ export function FooterNav(){
     return(
         <nav aria-label="Main Navigation" role="navigation" >
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Women's</a></li>
-                <li><a href="#">Men's</a></li>
-                <li><a href="#">On the Street</a></li>
-                <li><a href="#">The Catwalk</a></li>
-                <li><a href="#">AdWatch</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Tips</a></li>  
+                <li><a href="/Home">Home</a></li>
+                <li><a href="/Women">Women's</a></li>
+                <li><a href="/Men">Men's</a></li>
+                <li><a href="/OnTheStreet">On the Street</a></li>
+                <li><a href="/TheCatwalk">The Catwalk</a></li>
+                <li><a href="/AdWatch">AdWatch</a></li>
+                <li><a href="/About">About</a></li>
+                <li><a href="/Tips">Tips</a></li>  
             </ul>
         </nav>
     )
